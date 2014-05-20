478d9425f3c8db3396a13d6a37daab4c39bf2e9b
David Mosher <dmosher@noip.com>
2014-05-20 10:46:45 -0500

    Updates LieTest to test the non-existent view method...

--- tests/LieTest.php
+++ tests/LieTest.php
@@ -46,6 +46,54 @@ class LieTest extends PHPUnit_Framework_TestCase
             'Did not add lie to system as expected'
         );
     }
+
+    public function testView()
+    {
+        /**
+         * Create a data source
+         * Create a fake response from data source
+         * Stub out the methods to return the data 
+         * Create a LieModel object 
+         * Run the view() method passing in an ID
+         * Verify it returns the expected data
+         */
+        $statement = $this->getMockBuilder('StdClass')
+            ->setMethods(['execute', 'fetch'])
+            ->getMock();
+        $statement->expects($this->once())
+            ->method('execute')
+            ->will($this->returnValue(true));
+        // PDO is configured to return associative arrays
+        $id = 'abcdefghijk';
+        $data = [
+            'id' => $id,
+            'contents' => 'This is not a spammy comment',
+            'entry_date' => time(),   
+        ];
+        $statement->expects($this->once())
+            ->method('fetch')
+            ->will($this->returnValue($data));
+
+        // create our mock db conncetion
+        $db = $this->getMockBuilder('FakePDO')
+            ->setMethods(['prepare'])
+            ->getMock();
+
+        $db->expects($this->once())
+            ->method('prepare')
+            ->will($this->returnValue($statement));
+
+        $lie = new LieModel();
+        $lie->setDb($db);
+
+        $lie_info = $lie->view($id);
+
+        $this->assertEquals(
+            $data,
+            $lie_info,
+            'We did not get back the lie info we were expecting'
+        );
+    }
 }
 
 class FakePDO extends PDO
